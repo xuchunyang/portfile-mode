6a2b5dbd1a0df5df0af87c43c08ee5af8b9cda9a
Chunyang Xu <chunyang@macports.org>
2015-08-13 01:45:15 +0800

    Create portfile-mode.el

---
+++ portfile-mode.el
@@ -0,0 +1,55 @@
+;;; portfile-mode.el --- Major mode for editing MacPorts Portfile files  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Chunyang Xu
+
+;; Author: Chunyang Xu <chunyang@macports.org>
+;; Keywords: MacPorts Portfile
+
+;; This file is not part of GNU Emacs.
+
+;; Simplified BSD License:
+;;
+;; Redistribution and use in source and binary forms, with or
+;; without modification, are permitted provided that the following
+;; conditions are met:
+;;
+;;    1. Redistributions of source code must retain the above
+;;       copyright notice, this list of conditions and the following
+;;       disclaimer.
+;;
+;;    2. Redistributions in binary form must reproduce the above
+;;       copyright notice, this list of conditions and the following
+;;       disclaimer in the documentation and/or other materials
+;;       provided with the distribution.
+;;
+;; This software is provided by Chunyang Xu "AS IS" and any express
+;; or implied warranties, including, but not limited to, the implied
+;; warranties of merchantability and fitness for a particular
+;; purpose are disclaimed.  In no event shall Chunyang Xu or
+;; contributors be liable for any direct, indirect, incidental,
+;; special, exemplary, or consequential damages (including, but not
+;; limited to, procurement of substitute goods or services; loss of
+;; use, data, or profits; or business interruption) however caused
+;; and on any theory of liability, whether in contract, strict
+;; liability, or tort (including negligence or otherwise) arising in
+;; any way out of the use of this software, even if advised of the
+;; possibility of such damage.
+;;
+;; The views and conclusions contained in the software and
+;; documentation are those of the authors and should not be
+;; interpreted as representing official policies, either expressed
+;; or implied, of Chunyang Xu.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+
+(provide 'portfile-mode)
+;;; portfile-mode.el ends here
